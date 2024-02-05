e6f1cc45ebf09ba115beadc6ef80e17687bc42b7
Eduardo  Belotto Evangelista <156801841+EduardoZB@users.noreply.github.com>
2024-02-05 15:15:19 +0100

    Update nat2tif_pipe.py

--- Python/nat2tiff/nat2tif_pipe.py
+++ Python/nat2tiff/nat2tif_pipe.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from osgeo import gdal
 from osgeo import osr
@@ -8,7 +9,7 @@ import matplotlib.pyplot as plt
 import tkinter.filedialog
 import time
 
-### 2º snippet
+### Open file dialog asking what .nat file to work with
 
 file = tkinter.filedialog.askopenfilename()
 # define reader
@@ -20,7 +21,7 @@ dataset_names = scn.all_dataset_names()
 # print available datasets
 print('\n'.join(map(str, dataset_names)))
 
-### 3er snippet
+### Select a specific area to display
 
 # create some information on the reference system
 area_id = "Spain"
@@ -42,72 +43,72 @@ area_extent = (llx,lly,urx,ury)
 area_def = pr.geometry.AreaDefinition(area_id, proj_id, description, proj_dict, width, height, area_extent)
 print(area_def)
 
-### 4º snippet CUIDADOOOO
+### Define the function that converts the .nat file to a series of .tif files containing the different dataset images
 
 def nat2tif(file, calibration, area_def, dataset, reader, outdir, label, dtype, radius, epsilon, nodata):
-  # open the file
-  scn = Scene(filenames = {reader: [file]})
-  # let us check that the specified data set is actually available
-  scn_names = scn.all_dataset_names()
-  # raise exception if dataset is not present in available names
-  if dataset not in scn_names:
-    raise Exception("Specified dataset is not available.")
-  # we need to load the data, different calibration can be chosen
-  scn.load([dataset], calibration=calibration)
-  # let us extract the longitude and latitude data
-  lons, lats = scn[dataset].area.get_lonlats()
-  # now we can apply a swath definition for our output raster
-  swath_def = pr.geometry.SwathDefinition(lons=lons, lats=lats)
-  # and finally we also extract the data
-  values = scn[dataset].values
-  # we will now change the datatype of the arrays
-  # depending on the present data this can be changed
-  lons = lons.astype(dtype)
-  lats = lats.astype(dtype)
-  values = values.astype(dtype)
-  # now we can already resample our data to the area of interest
-  values = pr.kd_tree.resample_nearest(swath_def, values,
+    # open the file
+    scn = Scene(filenames = {reader: [file]})
+    # let us check that the specified data set is actually available
+    scn_names = scn.all_dataset_names()
+    # raise exception if dataset is not present in available names
+    if dataset not in scn_names:
+        raise Exception("Specified dataset is not available.")
+    # we need to load the data, different calibration can be chosen
+    scn.load([dataset], calibration=calibration)
+    # let us extract the longitude and latitude data
+    lons, lats = scn[dataset].area.get_lonlats()
+    # now we can apply a swath definition for our output raster
+    swath_def = pr.geometry.SwathDefinition(lons=lons, lats=lats)
+    # and finally we also extract the data
+    values = scn[dataset].values
+    # we will now change the datatype of the arrays
+    # depending on the present data this can be changed
+    lons = lons.astype(dtype)
+    lats = lats.astype(dtype)
+    values = values.astype(dtype)
+    # now we can already resample our data to the area of interest
+    values = pr.kd_tree.resample_nearest(swath_def, values,
                                              area_def,
                                              radius_of_influence=radius, # in meters
                                              epsilon=epsilon,
                                              fill_value=False)
-  # we are going to check if the outdir exists and create it if it doesnt
-  if not os.path.exists(outdir):
-   os.makedirs(outdir)
-  # let us join our filename based on the input file's basename                                           
-  outname = os.path.join(outdir, os.path.basename(file)[:-4] + "_" + str(label) + ".tif")
-  # now we define some metadata for our raster file
-  cols = values.shape[1]
-  rows = values.shape[0]
-  pixelWidth = (area_def.area_extent[2] - area_def.area_extent[0]) / cols
-  pixelHeight = (area_def.area_extent[1] - area_def.area_extent[3]) / rows
-  originX = area_def.area_extent[0]
-  originY = area_def.area_extent[3] 
-  # here we actually create the file
-  driver = gdal.GetDriverByName("GTiff")
-  outRaster = driver.Create(outname, cols, rows, 1)
-  # writing the metadata
-  outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
-  # creating a new band and writting the data
-  outband = outRaster.GetRasterBand(1)
-  outband.SetNoDataValue(nodata) #specified no data value by user
-  outband.WriteArray(np.array(values)) # writting the values
-  outRasterSRS = osr.SpatialReference() # create CRS instance
-  outRasterSRS.ImportFromEPSG(4326) # get info for EPSG 4326
-  outRaster.SetProjection(outRasterSRS.ExportToWkt()) # set CRS as WKT
-  # clean up
-  outband.FlushCache()
-  outband = None
-  outRaster = None
+    # we are going to check if the outdir exists and create it if it doesnt
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    # let us join our filename based on the input file's basename                                           
+    outname = os.path.join(outdir, os.path.basename(file)[:-4] + "_" + str(label) + ".tif")
+    # now we define some metadata for our raster file
+    cols = values.shape[1]
+    rows = values.shape[0]
+    pixelWidth = (area_def.area_extent[2] - area_def.area_extent[0]) / cols
+    pixelHeight = (area_def.area_extent[1] - area_def.area_extent[3]) / rows
+    originX = area_def.area_extent[0]
+    originY = area_def.area_extent[3] 
+    # here we actually create the file
+    driver = gdal.GetDriverByName("GTiff")
+    outRaster = driver.Create(outname, cols, rows, 1)
+    # writing the metadata
+    outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+    # creating a new band and writting the data
+    outband = outRaster.GetRasterBand(1)
+    outband.SetNoDataValue(nodata) #specified no data value by user
+    outband.WriteArray(np.array(values)) # writting the values
+    outRasterSRS = osr.SpatialReference() # create CRS instance
+    outRasterSRS.ImportFromEPSG(4326) # get info for EPSG 4326
+    outRaster.SetProjection(outRasterSRS.ExportToWkt()) # set CRS as WKT
+    # clean up
+    outband.FlushCache()
+    outband = None
+    outRaster = None
   
-#4º snippet
+### Call the above function and specify the folder and file names of the output, including the date and time for sorting purposes
 
 nowday = time.strftime('%d%m',time.gmtime())
 nowutc = time.strftime('%H_%M',time.gmtime())
 outdirv = './output'+nowday
 
 for i in dataset_names: #a ver que tal responde esto
-  nat2tif(file = file, 
+    nat2tif(file = file, 
         calibration = "radiance",  
         area_def = area_def,  
         dataset = i, 
@@ -118,6 +119,8 @@ for i in dataset_names: #a ver que tal responde esto
         radius = 16000, 
         epsilon = .5, 
         nodata = -3.4E+38)
+
+### Takes the individual .tif files and plot them with matplotlib. Every time this script is called, it output new .png filenames but overwrites the former .tif files inside the folder in order to save space.
 
 direction = os.fsencode(outdirv)
 
